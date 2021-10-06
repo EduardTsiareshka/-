fd6082bee784aabb094dbb4e23c17f314e4f2f42
EduardTsiareshka <91200203+EduardTsiareshka@users.noreply.github.com>
2021-10-06 16:22:52 +0300

    Add project files.

---
+++ целое число/целое число.cpp
@@ -0,0 +1,17 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    int n,tot=0;
+    do
+    {
+        cout << "enter any integer (to stop enter 0) \n";
+        cin >> n;
+        tot = tot + n;
+        cout << n << ",";
+    } 
+    while (n != 0);
+    cout << tot << endl;
+    return 0;
+}
